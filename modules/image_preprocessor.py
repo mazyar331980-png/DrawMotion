b7324207be4b2d95b758e0bb0a3834b8ae91668b
mazyar331980-png <mazyar331980@gmail.com>
2026-07-29 22:30:31 +0300

    Implement image preprocessing function
    Add image preprocessing function to read, convert, and save images.

--- modules/image_preprocessor.py
+++ modules/image_preprocessor.py
@@ -1,1 +1,21 @@
+import cv2
 
+def preprocess(image_path):
+    image = cv2.imread(image_path)
+
+    if image is None:
+        print("Помилка відкриття зображення.")
+        return None
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    result_path = "output/preprocessed.png"
+
+    cv2.imwrite(result_path, blur)
+
+    print("Попередня обробка завершена.")
+    print(f"Файл збережено: {result_path}")
+
+    return result_path
