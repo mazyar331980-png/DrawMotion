064de6709b2c631bd52e518f9a652a7b1c0cd909
mazyar331980-png <mazyar331980@gmail.com>
2026-07-29 22:36:14 +0300

    Update DrawMotion Engine version and add preprocessing

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from modules.png_loader import load_image
+from modules.image_preprocessor import preprocess
+import os
 
 print("=" * 40)
-print("      DrawMotion Engine v0.1")
+print("      DrawMotion Engine v0.2")
 print("=" * 40)
 
 image = load_image()
 
 if image:
     print("✅ Зображення успішно завантажено.")
+
+    folder = "input/images"
+    files = [f for f in os.listdir(folder)
+             if f.lower().endswith((".png", ".jpg", ".jpeg"))]
+
+    image_path = os.path.join(folder, files[0])
+
+    preprocess(image_path)
+
 else:
     print("❌ Не вдалося завантажити зображення.")
