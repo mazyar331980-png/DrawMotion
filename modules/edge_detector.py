70cd09480682e8d65044be62f36868a27234fbc5
mazyar331980-png <mazyar331980@gmail.com>
2026-07-30 22:09:19 +0300

    Update edge_detector.py

--- modules/edge_detector.py
+++ modules/edge_detector.py
@@ -1,1 +1,18 @@
+import cv2
 
+def detect_edges(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    if image is None:
+        print("Помилка відкриття обробленого зображення.")
+        return None
+
+    edges = cv2.Canny(image, 50, 150)
+
+    output_path = "output/edges.png"
+    cv2.imwrite(output_path, edges)
+
+    print("Контури знайдено.")
+    print(f"Файл збережено: {output_path}")
+
+    return output_path
