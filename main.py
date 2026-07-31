3208ea723d05b0811260e1b6f395c7e7ff1adbc2
mazyar331980-png <mazyar331980@gmail.com>
2026-07-31 14:17:09 +0300

    v0.4 Main Update
    Updated version to v0.4 and added SVG generation.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from modules.png_loader import load_image
 from modules.image_preprocessor import preprocess
 from modules.edge_detector import detect_edges
+from modules.svg_generator import generate_svg
 import os
 
 print("=" * 40)
-print("      DrawMotion Engine v0.3")
+print("      DrawMotion Engine v0.4")
 print("=" * 40)
 
 image = load_image()
@@ -21,7 +22,10 @@ if image:
     preprocessed = preprocess(image_path)
 
     if preprocessed:
-        detect_edges(preprocessed)
+        edges = detect_edges(preprocessed)
+
+        if edges:
+            generate_svg(edges)
 
 else:
     print("❌ Не вдалося завантажити зображення.")
